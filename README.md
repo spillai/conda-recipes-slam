8177ef6780dd293a1b303a3f1e4aaa119f15c351
Sudeep Pillai <spillai@csail.mit.edu>
2016-09-30 01:30:20 -0400

    updates to gtsam3, and 4 recipes

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 https://github.com/jakevdp/conda-recipes
 
 conda install -c asmeurer emacs=24.5
+conda install -c menpo eigen=3.2.7
--- gtsam4/build.sh
+++ gtsam4/build.sh
@@ -6,26 +6,21 @@ else
     PY_LIB="libpython2.7.so"
 fi
 
-# Install gtsam
 mkdir build
 cd build
 cmake                                                               \
-    -DPYTHON_EXECUTABLE=${PREFIX}/bin/python                        \
-    -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python2.7/               \
-    -DPYTHON_LIBRARY=${PREFIX}/lib/$PY_LIB                          \
-    -DPYTHON_PACKAGES_PATH=${PREFIX}/lib/python2.7/site-packages/   \
+    -DGTSAM_ALLOW_DEPRECATED_SINCE_V4=OFF                           \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}                                \
     -DGTSAM_WITH_EIGEN_MKL=ON                                       \
     -DGTSAM_WITH_EIGEN_MKL_OPENMP=ON                                \
     -DGTSAM_BUILD_EXAMPLES_ALWAYS=OFF                               \
     -DGTSAM_BUILD_TESTS=OFF                                         \
+    -DGTSAM_BUILD_PYTHON=ON                                         \
     ..
 make -j 6
 make install
 
-# Install pywrappers
-cmake \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    ../pywrappers
-make -j 6
-make install
+    # -DPYTHON_EXECUTABLE=${PREFIX}/bin/python                        \
+    # -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python2.7/               \
+    # -DPYTHON_LIBRARY=${PREFIX}/lib/$PY_LIB                          \
+    # -DPYTHON_PACKAGES_PATH=${PREFIX}/lib/python2.7/site-packages/   \
