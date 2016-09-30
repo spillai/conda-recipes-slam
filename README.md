afdd85dfced6bc0a782e3b5b3f7796168d660f3a
Sudeep Pillai <spillai@csail.mit.edu>
2016-09-29 23:52:37 -0400

    working gtsam compilation with 4.0

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# conda-recipes-slam
+tar -xvzf gtsam-3.2.1.tgz
+cd gtsam-3.2.1
+
+
+mkdir build
+cd build
+cmake                                                               \
+    -DPYTHON_EXECUTABLE=$PREFIX/bin/python                          \
+    -DPYTHON_INCLUDE_DIR=$PREFIX/include/python2.7/                 \
+    -DPYTHON_LIBRARY=$PREFIX/lib/$PY_LIB                            \
+    -DPYTHON_PACKAGES_PATH=$PREFIX/lib/python2.7/site-packages/     \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX                                  \
+    -DGTSAM_WITH_EIGEN_MKL=OFF                                      \
+    -DGTSAM_WITH_EIGEN_MKL_OPENMP=OFF                               \
+    -DGTSAM_BUILD_EXAMPLES_ALWAYS=OFF                               \
+    -DGTSAM_BUILD_TESTS=OFF                                         \
+    ..
+make -j 4
+make install
--- gtsam/build.sh
+++ gtsam/build.sh
@@ -9,18 +9,18 @@ fi
 mkdir build
 cd build
 cmake                                                               \
-    -DPYTHON_EXECUTABLE=${PREFIX}/bin/python                        \
-    -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python2.7/               \
-    -DPYTHON_LIBRARY=${PREFIX}/lib/$PY_LIB                          \
-    -DPYTHON_PACKAGES_PATH=${PREFIX}/lib/python2.7/site-packages/   \
+    -DGTSAM_ALLOW_DEPRECATED_SINCE_V4=OFF                           \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}                                \
-    -DCMAKE_OSX_ARCHITECTURES=                                      \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=                                  \
-    -DCMAKE_OSX_SYSROOT=                                            \
     -DGTSAM_WITH_EIGEN_MKL=ON                                       \
     -DGTSAM_WITH_EIGEN_MKL_OPENMP=ON                                \
     -DGTSAM_BUILD_EXAMPLES_ALWAYS=OFF                               \
     -DGTSAM_BUILD_TESTS=OFF                                         \
+    -DGTSAM_BUILD_PYTHON=ON                                         \
     ..
 make -j 6
 make install
+
+    # -DPYTHON_EXECUTABLE=${PREFIX}/bin/python                        \
+    # -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python2.7/               \
+    # -DPYTHON_LIBRARY=${PREFIX}/lib/$PY_LIB                          \
+    # -DPYTHON_PACKAGES_PATH=${PREFIX}/lib/python2.7/site-packages/   \
