61eb7e45ab84eee4ccf139dcded364cdb28c142e
Sudeep Pillai <spillai@csail.mit.edu>
2016-10-04 01:39:06 -0400

    updates to activate deactivate

--- pygtsam/activate.sh
+++ pygtsam/activate.sh
@@ -1,8 +1,11 @@
 echo "--------------------------------------------------------------"
 echo "Activating pygtsam: $CONDA_PREFIX"
 export _PYGTSAM_PYTHONPATH_BACKUP=$PYTHONPATH
+export _PYGTSAM_LD_LIBRARY_PATH_BACKUP=$LD_LIBRARY_PATH
 export PYTHONPATH=$PYTHONPATH:$CONDA_PREFIX/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib
 
 echo "Exported variables: "
 echo "PYTHONPATH: $PYTHONPATH"
+echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
 echo "--------------------------------------------------------------"
--- pygtsam/deactivate.sh
+++ pygtsam/deactivate.sh
@@ -1,5 +1,7 @@
 echo "--------------------------------------------------------------"
 echo "Deactivating pygtsam: $CONDA_PREFIX"
 export PYTHONPATH=$_PYGTSAM_PYTHONPATH_BACKUP
+export LD_LIBRARY_PATH=$_PYGTSAM_LD_LIBRARY_PATH_BACKUP
 unset _PYGTSAM_PYTHONPATH_BACKUP
+unset _PYGTSAM_LD_LIBRARY_PATH_BACKUP
 echo "--------------------------------------------------------------"
