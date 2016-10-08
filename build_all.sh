3a735ff9583c5fdb9c9f61bc53f37aaf6b6489bc
Sudeep Pillai <spillai@csail.mit.edu>
2016-10-08 13:39:56 -0400

    updates to slam recipes

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,14 @@
 conda config --set anaconda_upload no
 
 conda build isam
+conda install --use-local isam -y
+
 conda build pyisam
+conda install --use-local pyisam -y
 
 conda build gtsam
+conda install --use-local gtsam -y
+
 conda build pygtsam
+conda install --use-local pygtsam -y
+
--- gtsam/build.sh
+++ gtsam/build.sh
@@ -17,10 +17,3 @@ cmake                                                               \
     ..
 make -j $CPU_COUNT
 make install
-
-# -DPYTHON_EXECUTABLE=${PREFIX}/bin/python                        \
-    # -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python2.7/               \
-    # -DPYTHON_LIBRARY=${PREFIX}/lib/$PY_LIB                          \
-    # -DPYTHON_PACKAGES_PATH=${PREFIX}/lib/python2.7/site-packages/   \
-#     -DGTSAM_ALLOW_DEPRECATED_SINCE_V4=OFF                           \
-    # -DGTSAM_BUILD_PYTHON=ON                                         \
