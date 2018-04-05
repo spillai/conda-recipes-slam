31758c0eb8a94c40ad059971a3b1f07a85c40b94
Sudeep Pillai <spillai@csail.mit.edu>
2018-04-05 00:21:02 -0400

    adding suitesparse to build time deps for isam

--- build_all.sh
+++ build_all.sh
@@ -1,12 +1,12 @@
 conda build isam
 conda install --use-local isam -y
 
-conda build pyisam
-conda install --use-local pyisam -y
+# conda build pyisam
+# conda install --use-local pyisam -y
 
-conda build gtsam
-conda install --use-local gtsam -y
+# conda build gtsam
+# conda install --use-local gtsam -y
 
-conda build pygtsam
-conda install --use-local pygtsam -y
+# conda build pygtsam
+# conda install --use-local pygtsam -y
 
